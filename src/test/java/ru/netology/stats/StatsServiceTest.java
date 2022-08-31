83d3b15006a1fd416b9cf7ef3feb5e3d60a5f7b0
Adel-black <trifonova.alina@mail.ru>
2022-08-31 12:35:34 +1000

    init

---
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -0,0 +1,52 @@
+package ru.netology.stats;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void sumTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.sum(sales);
+        long expected = 180;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void averageTest() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long actual = service.average(sales);
+        long expected = 15;
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void shouldFindBetweenEnds() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedMonth = 9;
+        long actualMonth = service.minSales(sales);
+
+        Assertions.assertEquals(expectedMonth, actualMonth);
+    }
+
+    @Test
+
+    public void shouldFindBetweenEndsMax() {
+        StatsService service = new StatsService();
+        long[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        long expectedMonth = 8;
+        long actualMonth = service.maxSales(sales);
+
+        Assertions.assertEquals(expectedMonth, actualMonth);
+    }
+
+
+}
